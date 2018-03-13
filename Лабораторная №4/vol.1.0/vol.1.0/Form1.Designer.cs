a060a16b9a867b591169a68a0586788d2f19e812
Andrew <l0nelysp7@gmail.com>
2018-03-14 00:05:56 +0300

    Add files via upload
    Something...

---
+++ Лабораторная №4/vol.1.0/vol.1.0/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace vol._1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.List = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(336, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Number of employees";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button2.Location = new System.Drawing.Point(336, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button3.Location = new System.Drawing.Point(206, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button4.Location = new System.Drawing.Point(206, 59);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button5.Location = new System.Drawing.Point(206, 105);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 27);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button6.Location = new System.Drawing.Point(336, 105);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(149, 27);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Insert with changes";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button7.Location = new System.Drawing.Point(336, 152);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(149, 27);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Delete(new type)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button8.Location = new System.Drawing.Point(12, 270);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(149, 27);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Create new table";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.Font = new System.Drawing.Font("Monotype Corsiva", 9F, System.Drawing.FontStyle.Italic);
+            this.button9.Location = new System.Drawing.Point(12, 314);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(149, 27);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Delete new table";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // List
+            // 
+            this.List.BackColor = System.Drawing.Color.GhostWhite;
+            this.List.Cursor = System.Windows.Forms.Cursors.Default;
+            this.List.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.List.FormattingEnabled = true;
+            this.List.ItemHeight = 16;
+            this.List.Location = new System.Drawing.Point(12, 12);
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(188, 228);
+            this.List.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.Location = new System.Drawing.Point(506, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(264, 22);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBox2.Location = new System.Drawing.Point(506, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBox3.Location = new System.Drawing.Point(506, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(264, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBox4.Location = new System.Drawing.Point(506, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(264, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(782, 365);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.List);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ListBox List;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Лабораторная №4/vol.1.0/vol.1.0/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace vol._1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn = new SqlConnection(@"Data Source=DESKTOP-II3EQ3T;Initial Catalog= Кадры; Integrated Security = True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlCommand cmd = new SqlCommand();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "select sum([Population]) from  Department";
+            textBox1.Text = "General number of employees =  " + Convert.ToString(cmd.ExecuteScalar());
+            cnn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.CommandText = "select NameDepartment from  Department";
+            cmd.Connection = cnn;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                List.Items.Add(reader[0].ToString());
+            }
+
+            cnn.Close();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "select NameDepartment from  Department order by Population DESC";
+            textBox2.Text = "Most amount of employees - " + Convert.ToString(cmd.ExecuteScalar());
+            cnn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "insert into  Department values (10,1,3,'Security','Nevsky23', 'sc@trump.com', '5160609')";
+            cmd.ExecuteScalar();
+            cnn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cnn.Open(); cmd.Connection = cnn;
+            cmd.CommandText = "update  Department set Adress = 'Nevsky 153' where [Population] > 10";
+            cmd.ExecuteScalar();
+            cnn.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "delete from  Department where [Population] < 10";
+            cmd.ExecuteScalar();
+            cnn.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "insert into  Department values (3,1," + textBox3.Text + ",'Security','Nevsky 23','sc@trump.com', '5160609')";
+            textBox3.Text = "Changes = " + Convert.ToString(cmd.ExecuteNonQuery());
+            cnn.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "delete from  Department where Population = " + textBox4.Text;
+            textBox4.Text = "Deleted = " + Convert.ToString(cmd.ExecuteNonQuery());
+            cnn.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "create table Day (DayName int primary key, Sales int) ";
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "drop table Day";
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
